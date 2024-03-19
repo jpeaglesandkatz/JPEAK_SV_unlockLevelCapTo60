f65fed7a509bfd01900fd7476fec27b6158fd94d
jpeaglesandkatz <jpbinfo@proton.me>
2024-03-19 18:48:56 +0100

    Add project files.

---
+++ JPEAK_SV_unlockLevelCapTo60/JPEAK_SV_unlockLevelCapTo60Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace JPEAK_SV_unlockLevelCapTo60
+{
+    
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class JPEAK_SV_unlockLevelCapTo60Plugin : BaseUnityPlugin
+    {
+
+        private const string MyGUID = "com.jpb.JPEAK_SV_unlockLevelCapTo60";
+        private const string PluginName = "JPEAK_SV_unlockLevelCapTo60";
+        private const string VersionString = "1.0.0";
+
+        public static string CfgMaxLevelKey = "Max Level Cap";
+        
+        
+        public static ConfigEntry<int> CfgMaxLevel;
+        
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake()
+        {
+
+            CfgMaxLevel = Config.Bind("Max level",
+                CfgMaxLevelKey,
+                60,
+                new ConfigDescription("Set Max level Cap",
+                    new AcceptableValueRange<int>(50, 100)));
+
+            // Apply all of our patches
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.CreateAndPatchAll(typeof(JPEAK_SV_unlockLevelCapTo60Plugin), null);
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+                        
+            Log = Logger;
+        }
+
+
+        [HarmonyPatch(typeof(PChar),"UpdateChar")]
+        [HarmonyPrefix]
+        static bool UpdateCharprefix(ref int ___maxLevel)
+        {
+            ___maxLevel = CfgMaxLevel.Value;
+            return true;
+
+        }
+
+    }
+}
